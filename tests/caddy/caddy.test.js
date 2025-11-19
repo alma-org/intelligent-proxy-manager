6900f25ed345a010e2a56fa9be9a24016c149aeb
dangalcan <megamagolas@gmail.com>
2025-11-19 11:04:05 +0100

    tests: add http redirection to https in Caddy tests

--- tests/caddy/caddy.test.js
+++ tests/caddy/caddy.test.js
@@ -1,10 +1,12 @@
 import { describe, it, beforeAll, afterAll, expect } from "vitest";
 import { startCaddy } from "./helpers/startCaddyContainer.js";
 import { waitForHttps } from "./helpers/waitForHttps.js";
+import { waitForHttp } from "./helpers/waitForHttp.js";
 
 describe("Caddy HTTPS", () => {
   let container;
   let httpsPort;
+  let httpPort;
 
   beforeAll(async () => {
     const backendPort = process.env.TEST_BACKEND_PORT;
@@ -12,6 +14,7 @@ describe("Caddy HTTPS", () => {
     const started = await startCaddy({ backendPort });
     container = started.container;
     httpsPort = started.httpsPort;
+    httpPort = started.httpPort
 
     await waitForHttps({ port: httpsPort, timeout: 15000 });
   });
@@ -26,5 +29,20 @@ describe("Caddy HTTPS", () => {
     expect(res.statusCode).toBe(404);
   });
 
+  it("should redirect HTTP to HTTPS", async () => {
+    const res = await waitForHttp({ port: httpPort });
+
+    // Caddy usually redirects with these codes
+    // when forcing HTTPS
+    expect([301, 308]).toContain(res.statusCode);
+
+    const location = res.headers["location"];
+    expect(location).toMatch(/^https:\/\//);
+  });
+
   
 });
+
+
+
+
