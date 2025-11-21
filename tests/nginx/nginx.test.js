34718b1119bca865609e915e4841fc589f5751f4
dangalcan <megamagolas@gmail.com>
2025-11-21 11:16:32 +0100

    test: add tests for undefined apikey and with no apikey in headers

--- tests/nginx/nginx.test.js
+++ tests/nginx/nginx.test.js
@@ -1,5 +1,6 @@
 import { describe, it, expect, beforeAll, afterAll } from "vitest";
 import net from "net";
+import http from "http";
 import { startNginxFromFile } from "./helpers/startNginxFromFile.js";
 
 describe("Nginx runs with provided config", () => {
@@ -7,7 +8,10 @@ describe("Nginx runs with provided config", () => {
   let port;
 
   beforeAll(async () => {
-    instance = await startNginxFromFile({nginxConfPath: process.env.NGINX_FILE_TO_TEST, nginxPort: process.env.NGINX_PORT});
+    instance = await startNginxFromFile({
+      nginxConfPath: process.env.NGINX_FILE_TO_TEST,
+      nginxPort: process.env.NGINX_PORT,
+    });
     port = instance.httpPort;
   });
 
@@ -20,7 +24,7 @@ describe("Nginx runs with provided config", () => {
   });
 
   it("should be listening on 8080 (mapped)", async () => {
-    const isListening = await new Promise(resolve => {
+    const isListening = await new Promise((resolve) => {
       const socket = net.createConnection({ host: "127.0.0.1", port }, () => {
         socket.end();
         resolve(true);
@@ -30,6 +34,38 @@ describe("Nginx runs with provided config", () => {
     });
 
     expect(isListening).toBe(true);
+  });
+
+  it("should return 401 because no api-key was sent in the headers", async () => {
+    const res = await new Promise((resolve) => {
+      http.get(
+        {
+          hostname: "127.0.0.1",
+          port,
+          path: "/testPath",
+        },
+        resolve
+      );
+    });
+    expect(res.statusCode).toBe(401);
+  });
+
+  it("should return 403 an invalid api-key was sent", async () => {
+    const res = await new Promise((resolve) => {
+      http.get(
+        {
+          hostname: "127.0.0.1",
+          port,
+          path: "/testPath",
+          headers: {
+            apikey: "invalid-api-key",
+          },
+        },
+        resolve
+      );
+    });
+
+    expect(res.statusCode).toBe(403);
   });
 
 
